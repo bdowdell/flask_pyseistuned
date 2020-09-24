d52f07bdfdbe06179e5b79cf20b0da54a77e6293
Ben Dowdell <ben.dowdell87@gmail.com>
2020-09-23 21:55:07 -0500

    implements validators for all input fields, including custom validator for frequency

--- pyseistuned/forms.py
+++ pyseistuned/forms.py
@@ -18,7 +18,7 @@ limitations under the License.
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, IntegerField, DecimalField, RadioField, SelectField
-from wtforms.validators import DataRequired, Email, Length, ValidationError
+from wtforms.validators import ValidationError, DataRequired, Email, Length, NumberRange
 
 
 class NotEqualTo(object):
@@ -34,6 +34,7 @@ class NotEqualTo(object):
         interpolated with `%(other_label)s` and `$(other_name)s` to provide a
         more helpful error.
     """
+
     def __init__(self, fieldname, message=None):
         self.fieldname = fieldname
         self.message = message
@@ -54,6 +55,69 @@ class NotEqualTo(object):
             raise ValidationError(message % d)
 
 
+class ValidateFrequency(object):
+    """
+    Custom validator for frequency field.  Requires input of wavelet type field.
+
+    Paramaters
+    ----------
+    fieldname : str
+        The name of the wavelet type form field
+    message : str
+        default message to pass
+    """
+
+    def __init__(self, fieldname, message=None):
+        self.fieldname = fieldname
+        self.message = message
+
+    def __call__(self, form, field):
+        message = self.message
+        try:
+            wavelet_type = form[self.fieldname]
+        except KeyError:
+            raise ValidationError(field.gettext("Invalid field name '%s'.") % self.fieldname)
+        if int(wavelet_type.data) == 1:
+            # Ormsby wavelet type has value of 1, so check this case first.
+            try:
+                freqs = [int(x) for x in field.data.split(',')]  # Ormsby filter should be entered as F1, F2, F3, F4
+                # check that four frequencies are entered
+                if len(freqs) != 4:
+                    message = "Wrong number of frequency corner points supplied or not properly formatted."
+                    raise ValidationError
+                # check that all frequencies are positive
+                for x in freqs:
+                    if x < 0:
+                        message = "Negative frequency entered.  All frequencies need to be positive."
+                        raise ValidationError
+                # check that frequencies increase in size
+                if freqs[0] < freqs[1] < freqs[2] < freqs[3]:
+                    pass
+                else:
+                    message = "Frequencies should increase in value: F1 < F2 < F3 < F4."
+                    raise ValidationError
+            except ValidationError:
+                if message is None:
+                    message = "Incorrect specification for desired wavelet.  Please refer to hover tooltip."
+                raise ValidationError(message)
+        else:
+            # Ricker wavelet type has value of 0, so check that case second.
+            try:
+                freqs = [int(x) for x in field.data.split(',')]  # Ricker filter should only have one frequency Fc
+                # check that only one frequency is entered
+                if len(freqs) != 1:
+                    message = "Too many frequencies entered. Ricker wavelet only takes one frequency."
+                    raise ValidationError
+                # check that all frequencies are positive
+                if freqs[0] < 0:
+                    message = "Negative frequency entered. Only positive values accepted."
+                    raise ValidationError
+            except ValidationError:
+                if message is None:
+                    message = "Incorrect specification for desired wavelet.  Please refer to hover tooltip."
+                raise ValidationError(message)
+
+
 class ContactForm(FlaskForm):
     """Contact form."""
     name = StringField('Name', validators=[DataRequired()])
@@ -71,18 +135,25 @@ class ContactForm(FlaskForm):
 
 class TuningWedgeForm(FlaskForm):
     """Inputs for calculating tuning wedge"""
-    layer_1_vp = IntegerField('vp_1')  # layer_1 re-used for layer_3
-    layer_1_dens = DecimalField('rho_1')  # layer_1 re-used for layer_3
+    layer_1_vp = IntegerField('vp_1', validators=[DataRequired(), NumberRange(min=0, max=20000)])
+    layer_1_dens = DecimalField('rho_1', validators=[DataRequired(), NumberRange(min=1.0, max=5.0)])
     layer_1_impedance = IntegerField('imp_1')
-    layer_2_vp = IntegerField('vp_2')
-    layer_2_dens = DecimalField('rho_2')
+    # layer_3 properties will be the same as layer_1
+    layer_2_vp = IntegerField('vp_2', validators=[DataRequired(), NumberRange(min=0, max=20000)])
+    layer_2_dens = DecimalField('rho_2', validators=[DataRequired(), NumberRange(min=1.0, max=5.0)])
     layer_2_impedance = IntegerField('imp_2', validators=[NotEqualTo('layer_1_impedance', message='Impedances are the '
-                                                                                                'same and will not '
-                                                                                                'produce a '
-                                                                                                'reflection.')])
+                                                                                                  'same and will not '
+                                                                                                  'produce a '
+                                                                                                  'reflection.')])
     vp_units = RadioField(label='Vp Units', choices=[(0, 'm/s'), (1, 'ft/s')])
     wv_type = SelectField(label='Wavelet', choices=[(0, 'Ricker'), (1, 'Ormsby')])
-    frequency = StringField('Frequency (Hz)')
-    wv_length = DecimalField('Length (s)', places=3)
-    wv_dt = DecimalField('dt (s)', places=3)
+    frequency = StringField('Frequency (Hz)', validators=[DataRequired(), ValidateFrequency('wv_type')])
+    wv_length = DecimalField('Length (s)', places=3, validators=[DataRequired(), NumberRange(min=0.01, max=1.0)])
+    wv_dt = DecimalField('dt (s)',
+                         places=3,
+                         validators=[
+                                     DataRequired(),
+                                     NumberRange(min=0.0009, max=0.0041, message="dt must be between 0.001 and 0.004")
+                                    ]
+                         )
     submit = SubmitField('Calculate')
