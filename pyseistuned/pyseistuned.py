cbd65249aad2f76d67d4aa02bee3727b3375e038
Ben Dowdell <ben.dowdell87@gmail.com>
2020-10-16 08:49:10 -0500

    initializes values to 0 for initial page load ... may be changed in later commit

--- pyseistuned/pyseistuned.py
+++ pyseistuned/pyseistuned.py
@@ -38,12 +38,12 @@ mail = Mail(app)
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = TuningWedgeForm(
-        layer_1_vp=session.get('vp_1')/1000, layer_1_dens=session.get('rho_1')/1000,
-        layer_2_vp=session.get('vp_2')/1000, layer_2_dens=session.get('rho_2')/1000,
-        layer_3_vp=session.get('vp_3')/1000, layer_3_dens=session.get('rho_3')/1000,
+        layer_1_vp=session.get('vp_1', 0)/1000, layer_1_dens=session.get('rho_1', 0)/1000,
+        layer_2_vp=session.get('vp_2', 0)/1000, layer_2_dens=session.get('rho_2', 0)/1000,
+        layer_3_vp=session.get('vp_3', 0)/1000, layer_3_dens=session.get('rho_3', 0)/1000,
         vp_units=session.get('vp_units', 0), wv_type=session.get('wv_type', 0.100),
-        frequency=session.get('freq'), wv_length=session.get('wv_len', 0.001)/1000,
-        wv_dt=session.get('wv_dt')/1000
+        frequency=session.get('freq', 0), wv_length=session.get('wv_len', 100)/1000,
+        wv_dt=session.get('wv_dt', 1)/1000
     )
     if form.validate_on_submit():
         # capture inputs to session dictionary ... decimals cannot be JSONified, so multiply by 1000 and cast to int
