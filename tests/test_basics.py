86b8e9459d55a69954c97a28ad999f174fd1fcba
Ben Dowdell <ben.dowdell87@gmail.com>
2020-10-22 20:22:14 -0500

    adds initial unit tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -16,15 +16,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os
+import unittest
+from flask import current_app
 from app import create_app
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+class BasicTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
 
-@app.cli.command()
-def test():
-    """Run the unit tests"""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    def tearDown(self):
+        self.app_context.pop()
+
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+
+    def test_app_is_test(self):
+        self.assertTrue(current_app.config['TESTING'])
