2c4ab9d25bbb5ed218288c934cf6ea428915c8d7
Ben Dowdell <ben.dowdell87@gmail.com>
2020-10-25 14:48:33 -0500

    adds unit tests for wavelet & amplitude spectrum plotting scripts and refactors test names to keep ordered

---
+++ tests/test_02_bokeh_wavelet.py
@@ -0,0 +1,36 @@
+"""
+Copyright 2020, Benjamin L. Dowdell
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+from app.main import wedgebuilder as wb
+from app.main import bokeh_wavelet as bwv
+from bokeh.plotting import figure
+import numpy as np
+
+
+class BokehWaveletTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.duration = 0.200
+        self.dt = 0.001
+
+    def test_00_plot_wavelet(self):
+        self.assertIsInstance(self.duration, float)
+        self.assertIsInstance(self.dt, float)
+        wb_wavelet = wb.wavelet(self.duration, self.dt, w_type=0, f=[25])
+        self.assertIsInstance(wb_wavelet, np.ndarray)
+        wavelet_plot = bwv.plot_wavelet(wb_wavelet, self.duration)
+        self.assertIsInstance(type(wavelet_plot), type(figure.__class__))  # <class 'bokeh.plotting.figure.Figure'>
---
+++ tests/test_03_bokeh_amplitude_spectrum.py
@@ -0,0 +1,38 @@
+"""
+Copyright 2020, Benjamin L. Dowdell
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+from app.main import wedgebuilder as wb
+from app.main import bokeh_amplitude_spectrum as bas
+from bokeh.plotting import figure
+import numpy as np
+
+
+class BokehAmplitudeSpectrumTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.duration = 0.200
+        self.dt = 0.001
+
+    def test_00_plot_amplitude_spectrum(self):
+        self.assertIsInstance(self.duration, float)
+        self.assertIsInstance(self.dt, float)
+        wb_wavelet = wb.wavelet(self.duration, self.dt, w_type=0, f=[25])
+        self.assertIsInstance(wb_wavelet, np.ndarray)
+        self.assertIsInstance(bas.plot_amplitude_spectrum(wb_wavelet, self.dt), tuple)
+        spectrum, phase = bas.plot_amplitude_spectrum(wb_wavelet, self.dt)
+        self.assertIsInstance(type(spectrum), type(figure.__class__))
+        self.assertIsInstance(type(phase), type(figure.__class__))
