27b53132c311077832d02cd8d3784c856f205d06
Ben Dowdell <ben.dowdell87@gmail.com>
2020-11-01 17:43:33 -0600

    renames to test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -21,7 +21,7 @@ from app import create_app
 from app.main.forms import ContactForm, TuningWedgeForm
 
 
-class APITestCase(unittest.TestCase):
+class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
