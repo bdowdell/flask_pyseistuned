cc75684f189b161e8755cbdcf3e94b5bcccb7e5e
Ben Dowdell <ben.dowdell87@gmail.com>
2020-11-01 23:39:16 -0600

    adds bootstrap spinner to results page

--- app/static/js/results_scripts.js
+++ app/static/js/results_scripts.js
@@ -16,9 +16,8 @@ limitations under the License.
 */
 
 // Defines a timeout so that page content will not load until after Bokeh plots do
-$(function () {
-            setTimeout(function() {
-                // Show the div after 0.5s
-                $("#divDelay").show();
-            }, 500);
-        });
+// and hides bootstrap spinner upon load
+setTimeout(function() {
+    $("#divDelay").show();
+    $("#spinner").hide()
+}, 2000);
