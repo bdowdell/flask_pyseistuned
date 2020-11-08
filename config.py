7c9d1d77fa3918d1e1617558d91cb339b6ce5a5f
Ben Dowdell <ben.dowdell87@gmail.com>
2020-11-08 13:23:28 -0600

    adds configuration for production and unix server deployment

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'localhost')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 8025))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    PST_MAIL_SENDER = 'PySeisTuned Admin <admin@pyseistuned.com>'
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['ben@test.com']
@@ -27,13 +28,51 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    ADMINS = [os.environ.get('PST_ADMIN')]
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email errors to administrator
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.PST_MAIL_SENDER,
+            toaddrs=cls.ADMINS,
+            subject='PySeisTuned Application Error',
+            credentials=credentials,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler)
 
 
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'unix': UnixConfig,
     'default': DevelopmentConfig,
 }
