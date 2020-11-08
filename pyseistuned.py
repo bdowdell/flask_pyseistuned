aaf042c07256140b00bdbd381bda48c87c554a18
Ben Dowdell <ben.dowdell87@gmail.com>
2020-11-08 13:27:21 -0600

    loads .env file if it exists to set environment variables

--- pyseistuned.py
+++ pyseistuned.py
@@ -17,7 +17,12 @@ limitations under the License.
 """
 
 import os
+from dotenv import load_dotenv
 from app import create_app
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
